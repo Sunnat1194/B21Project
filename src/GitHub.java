816c5652225c060f277233da6686316e59e9f064
day1 <Sunnat1194@gmail.com>
2020-11-10 12:29:01 -0500

    add project new to java and it is so fun for learn

--- src/GitHub.java
+++ src/GitHub.java
@@ -1,5 +1,7 @@
 public class GitHub {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("how are you");
+        System.out.println("Im good");
     }
 }
